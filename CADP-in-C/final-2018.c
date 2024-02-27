f77bc3df1f91b3a0828391f3903a12f0b6a62e46
facundolubo <facundolubo@gmail.com>
2024-02-27 01:59:35 -0300

    resolvi sin entender como, no se pq la len es +2 respecto de la cantidad de char...

--- CADP-in-C/final-2018.c
+++ CADP-in-C/final-2018.c
@@ -16,101 +16,91 @@ typedef struct vectorChar {
 
 typedef struct charCounter {
     char c[(ASCII_z - ASCII_A) - (ASCII_a - ASCII_Z)];
-    int len[(ASCII_z - ASCII_A) - (ASCII_a - ASCII_Z)];
+    int len[(ASCII_z - ASCII_A) - (ASCII_a - ASCII_Z) + 2]; //this +2 solve the code, don't know why
+    int totalLen;
 } CharCounter;
 
-// Function to generate a random character between 'A' and 'z'
 char getRandomChar() {
     char c;
     int randInt = rand() % (ASCII_z + 1);
     if ((randInt >= ASCII_A && randInt <= ASCII_Z) || (randInt >= ASCII_a && randInt <= ASCII_z)) {
         c = (char)randInt;
     } else {
-        // If the generated character is not within the range, call getRandomChar recursively
         c = getRandomChar();
     }
     return c;
 }
 
-// Function to generate a random sorted array of characters
 VectorChar createArray() {
     VectorChar vChar;
+    vChar.len = 0;
 
-    srand(time(NULL)); // Seed the random number generator
+    srand(time(NULL));
 
-    // Generate random characters and store them in the array
-    int randLen = rand() % MAX_CHAR; // random length of the array
-    int nullLen = MAX_CHAR -  randLen;
+    int randLen = rand() % MAX_CHAR;
+    int nullLen = MAX_CHAR - randLen;
     printf("randLen: %d\nnullLen: %d\n", randLen, nullLen);
     while (vChar.len < randLen) {
-		char randChar = getRandomChar();
-		int insertPos = 0;
-
-		if (randChar < vChar.arrChar[insertPos]) {
-			vChar.len++;
-			for (int i = vChar.len; i > insertPos; i--) {
-				vChar.arrChar[i-1] = vChar.arrChar[i];
-			}
-			vChar.arrChar[insertPos] = randChar;
-		}
-		insertPos++;
-		printf("actPos: %d\n", insertPos);
-	}
-    for (int i = vChar.len; i < nullLen; i++) {
+        char randChar = getRandomChar();
+        int insertPos = 0;
+        while (insertPos < vChar.len && randChar > vChar.arrChar[insertPos]) {
+            insertPos++;
+        }
+        for (int i = vChar.len; i > insertPos; i--) {
+            vChar.arrChar[i] = vChar.arrChar[i - 1];
+        }
+        vChar.arrChar[insertPos] = randChar;
+        vChar.len++;
+    }
+    for (int i = vChar.len; i < MAX_CHAR; i++) {
         vChar.arrChar[i] = NULL_CHAR;
     }
-
     return vChar;
 }
 
-// Function to initialize the counter structure
 CharCounter initCounter() {
     CharCounter counter = {
-        .c = {0}, // Initialize all elements of c array to 0 initially
-        .len = {0} // Initialize all elements of len array to 0
+        .c = {0},
+        .len = {0},
+        .totalLen = 0
     };
-
-    // Initialize characters from 'A' to 'Z'
     for (int i = 0; i <= (ASCII_Z - ASCII_A); i++) {
-        counter.c[i] = 'A' + i;
+        counter.c[i] = ASCII_A + i;
+        counter.totalLen++;
     }
 
-    // Initialize characters from 'a' to 'z'
     for (int i = 0; i <= (ASCII_z - ASCII_a); i++) {
-        counter.c[i + (ASCII_Z - ASCII_A + 1)] = 'a' + i;
+        counter.c[counter.totalLen] = ASCII_a + i;
+        counter.totalLen++;
     }
-
+    printf("Counter total len: %d\n", counter.totalLen);
     return counter;
 }
 
-// Function to print the array of characters
-void printArr(VectorChar vChar) {
+void printArr(VectorChar *vChar) {
     printf("Array of characters:\n");
-    for (int i = 0; i < vChar.len; i++) {
-        printf("%c ", vChar.arrChar[i]);
+    for (int i = 0; i < vChar->len; i++) {
+        printf("%c", vChar->arrChar[i]);
     }
     printf("\n");
 }
 
-// Function to print the counter array
-void printCounter(CharCounter counter) {
-    printf("Counter array:\n");
-    for (int i = 0; i < (ASCII_z - ASCII_A) - (ASCII_a - ASCII_Z); i++) {
-        printf("%c: %d\n", counter.c[i], counter.len[i]);
+void printCounter(CharCounter *counter) {
+    printf("Counter len: %d\n", counter->totalLen);
+    for (int i = 0; i < counter->totalLen; i++) {
+        printf("%c: %d\n", counter->c[i], counter->len[i]);
     }
 }
 
-// Function to process the information and update the counter
 void proc_info(VectorChar vChar, CharCounter *counter) {
     int posVec = 0;
-
-    while (vChar.arrChar[posVec] != NULL_CHAR) {
+    while (posVec < vChar.len) {
         char actChar = vChar.arrChar[posVec];
         int posCt = 0;
         while (counter->c[posCt] != actChar) {
             posCt++;
         }
-        while ((vChar.arrChar[posVec] != NULL_CHAR) && (actChar == vChar.arrChar[posVec])) {
+        while (vChar.arrChar[posVec] != NULL_CHAR && actChar == vChar.arrChar[posVec]) {
             counter->len[posCt]++;
             posVec++;
         }
@@ -120,9 +110,9 @@ void proc_info(VectorChar vChar, CharCounter *counter) {
 int main() {
     VectorChar vChar = createArray();
     CharCounter counter = initCounter();
-    printArr(vChar);
+    printArr(&vChar);
+    printCounter(&counter);
     proc_info(vChar, &counter);
-    printCounter(counter);
+    printCounter(&counter);
     return 0;
 }
-
