74005e0200863e79d7c25c58255eb98baf834837
facundolubo <facundolubo@gmail.com>
2024-03-04 10:51:28 -0300

    mergesorteee

--- Aho/mergeSort.c
+++ Aho/mergeSort.c
@@ -1,39 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define DIML 20
-// Define the DefCell macro
-#define DefCell(EltType, CellType, ListType) \
-typedef struct CellType *ListType; \
-struct CellType { \
-    EltType data; \
-    ListType next; \
-}
 
-// Use the DefCell macro to define IntCell and IntList
-DefCell(int, IntCell, IntList);
+// Define the node structure
+typedef struct IntNode {
+    int data;
+    struct IntNode *next;
+} IntNode;
 
+typedef struct IntList { 
+    IntNode *head;
+    int len; 
+} IntList;
+
+// Function to generate a random integer between min and max
 int getRandomInt(int min, int max) {
-	int range = max-min;
-	int rnum = min + rand() % (range + 1);
-	return rnum;
+    return min + rand() % (max - min + 1);
 }
 
-void createList(int diml) {
-    IntList node = malloc(sizeof(struct IntCell)); // Use struct IntCell instead of IntCell
-    node->element = 10;
-    node->next = NULL;
+IntNode *insert(IntList *list, int num) {
+    IntNode *node = malloc(sizeof(IntNode));
+    if (node == NULL) {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    node->data = num;
+    node->next = list->head;
+    list->head = node;
+    list->len++;
+    return node;
 }
 
+IntList createList(int diml) {
+    IntList list;
+    list.head = NULL;
+    list.len = 0;
+    srand(time(NULL)); // Seed the random number generator
+
+    for (int i = 0; i < diml; i++) {
+        insert(&list, getRandomInt(0, 50));
+    }
+    return list;
+}
+
+void printList(IntList list) {
+    printf("Printing list ...\n");
+    IntNode *auxNode = list.head;
+    while (auxNode != NULL) {
+        printf("Value: %d\n", auxNode->data);
+        auxNode = auxNode->next;
+    }
+}
+
+IntList merge(IntList list1, IntList, list2) {
+	IntList list;
+	if (list1 == NULL) {
+		list = list2;
+	}
+	else if (list2 == NULL) {
+		list = list1;
+	}
+	else if (list1->data <= list2->data) {
+		list1->next = merge(list1->next, list2);
+		list = list1;
+	}
+	else {
+		list2->next = merge(list1, list2->next);
+		list =list2;
+	}
+	return list;
+}
 
 int main() {
-    // Create a linked list node
-    createList(DIML)
-    // Print the element of the node
-    printf("Element of node1: %d\n", node->element);
-
-    // Free the allocated memory
-    free(node);
-
+    // Create a linked list
+    IntList list = createList(DIML);
+    // Print the list
+    printList(list);
     return 0;
 }
