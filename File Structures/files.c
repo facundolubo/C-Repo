6a8bb07d1308b5867884106c9033fa7f565b70f1
Facundo Lubo <facundolubo@gmail.com>
2024-03-31 20:58:56 -0300

    testeando files

--- File Structures/files.c
+++ File Structures/files.c
@@ -11,25 +11,44 @@ int main() {
     int fd; // file descriptor
     char filename[20];
     char filecontent[256];
-    int choice = YES;
-    
-    
+    int choice;
+
+    printf("Do you want to create a new file (1) or read an existing file (0)? ");
+    scanf("%d", &choice);
+    getchar(); // Consume the newline character left in the input buffer
+
     if (choice == YES) {
         printf("Enter the name of the file to be created: \n");
-        gets(filename); // Using gets() to read the filename
+        fgets(filename, sizeof(filename), stdin); // Using fgets() for input
+        strtok(filename, "\n"); // Remove the trailing newline if present
         printf("Write the content of the file: \n");
-        gets(filecontent); 
-		fd = open(filename, O_CREAT | O_WRONLY, 0644);
-    }
-    
-    else {
+        fgets(filecontent, sizeof(filecontent), stdin);
+        strtok(filecontent, "\n");
+        fd = open(filename, O_CREAT | O_WRONLY, 0644);
+        if (fd == -1) {
+            perror("Failed to create file");
+            return 1;
+        }
+        write(fd, filecontent, strlen(filecontent));
+    } else if (choice == NO) {
         printf("Enter the name of the file to be read: ");
-		gets(filename); // Using gets() to read the filename
+        fgets(filename, sizeof(filename), stdin);
+        strtok(filename, "\n");
         fd = open(filename, O_RDONLY);
-		while (read(fd, &c, 1) != 0) {
-				write(STDOUT_FILENO, &c, 1);
-			}
-		}
+        if (fd == -1) {
+            perror("Failed to open file");
+            return 1;
+        }
+        while (read(fd, &c, 1) != 0) {
+            write(STDOUT_FILENO, &c, 1);
+        }
+		printf("\n");
+    } else {
+        printf("Invalid choice\n");
+        return 1;
+    }
+
     close(fd); // Close the file descriptor
     return 0;
 }
+
