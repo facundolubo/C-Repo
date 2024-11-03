458b0256c8cbacc96aedd8af43a134da2b088e58
facundolubo <facundolubo@gmail.com>
2024-11-03 15:59:33 -0300

    adsnjads

---
+++ The-C-Programming-Language/chap5/5-5.c
@@ -0,0 +1,16 @@
+/*Exercise 5-5. Write versions of the library functions strncpy, strncat, and strncmp, which
+operate on at most the first n characters of their argument strings. For example,
+strncpy(s,t,n) copies at most n characters of t to s. Full descriptions are in Appendix B.*/
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+my_strncpy(){}
+my_strncat(){}
+my_strncmp(){}
+
+int main () {
+	return 0;
+}
